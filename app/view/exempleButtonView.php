68d83e532664323906231cdc80acb6643f5c7e32
Faraux Yanis <yanis.faraux@gmail.com>
2025-12-05 01:10:45 +0100

    Exemple button

---
+++ app/view/exempleButtonView.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+
+<div class="container">
+        <button class="btn-dialog" data-perso="Panoramix">
+            coucou panoramix
+        </button>
+</div>
+
+<dialog id="monPopup">
+    <h2 id="popupTitre"></h2>
+    <div id="popupTexte"></div>
+    <form method="dialog"><button>Fermer</button></form>
+</dialog>
+
+
+<script src="assets/js/dialogue.js"></script>
+
+</body>
+</html>
