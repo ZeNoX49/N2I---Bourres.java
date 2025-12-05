11cd1124a436d0b95d6345fc810399c9efba6a98
MUSSET Thomas <thomas.musset.etu@univ-lemans.fr>
2025-12-05 05:46:41 +0100

    Début du formulaire

---
+++ app/view/formulaireView.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulaire</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+
+<div class="container">
+        <button class="btn-dialog" data-perso="Panoramix">
+            coucou panoramix
+        </button>
+</div>
+
+<dialog id="monPopup">
+    <h2 id="popupTitre"></h2>
+    <div id="popupTexte"></div>
+    <form method="dialog"><button>Fermer</button></form>
+</dialog>
+
+</body>
+</html>
