7a4913f47fdfd2593f8ab42aeb39a21c625da36d
Hugo <hugobard35@gmail.com>
2025-12-05 01:54:17 +0100

    Annulation

--- app/view/home.php
+++ app/view/home.php
@@ -3,80 +3,14 @@
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <div class="village">
-    <div class="lien-texte">
-        <a href="?action=dialogue/show&type=bureautique">MAISON bureautique</a>
-        <div class="contenu-dialogue" data-type="bureautique">
-            <h4 class="role">Le Doyen de la Bureautique</h4>
-            <p>Moi, je dis qu'il y a urgence à agir contre la dépendance structurelle ! Rendre mon matériel obsolète juste parce qu'ils arrêtent le support de Windows ? Jamais ! Notre village résiste en favorisant le réemploi ! On installe LibreOffice et Linux, c'est la voie pour un Numérique Responsable et Durable qui renforce l'autonomie technologique ! Je suis toujours pas dépassé, alors pourquoi le matos le serait ?</p>
-        </div>
-    </div>
-
-    <div class="lien-texte">
-        <a href="?action=dialogue/show&type=os">MAISON OS</a>
-        <div class="contenu-dialogue" data-type="os">
-            <h4 class="role">La Forge de l'OS</h4>
-            <p>Hein ! Cet ordinateur est encore bon ! Les Romains disent qu'il est 'obsolètum' juste parce que leur Windows 10 n'est plus supporté! Mais moi, je vous dis qu'avec un peu de bon sens et Linux, on peut lui donner une seconde vie ! Je suis le forgeron, je sais ce qu'est un outil qui dure! Vive le reconditionnement!</p>
-        </div>
-    </div>
-
-    <div class="lien-texte">
-        <a href="?action=dialogue/show&type=dessin">MAISON Dessin</a>
-        <div class="contenu-dialogue" data-type="dessin">
-            <h4 class="role">Maison Dessin</h4>
-            <p><strong>Personnage A :</strong> C'est pas possible ! Tu dessines encore tes menhirs sur cette tablette qui ne veut rien laisser sortir ? C'est un écosystème fermé, ça ! Une vraie garnison romaine !</p>
-            <p><strong>Personnage B :</strong> Ouïe ! Tu as raison, il me faut toujours une licence coûteuse pour exporter en bonne qualité. Mais que faire ?</p>
-            <p><strong>Personnage A :</strong> Passe à Krita ! Il est libre de droit, on peut le mettre sur toutes nos machines, même celles que l'Empire veut rendre obsolètes ! C'est l'esprit de résistance des gaulois mais surtout l'esprit de résistance numérique propre à NIRD !</p>
-        </div>
-    </div>
-
-    <div class="lien-texte">
-        <a href="?action=dialogue/show&type=ide">MAISON IDE</a>
-        <div class="contenu-dialogue" data-type="ide">
-            <h4 class="role">La tavern'IDE</h4>
-            <p>Mes amis, pour concocter une application web efficace et légère, pas besoin de solution coûteuse comme la suite JetBrains. Le secret est dans l'outil libre, comme VSCode. Il permet à chaque établissement scolaire de devenir autonome et de coconstruire sa solution a l'aide d'une grande quantité d'extension faite par la communauté pour la communauté. C'est comme pour la Guilde des Druides et c'est ça l'essence du NIRD !</p>
-        </div>
-    </div>
-
-    <div class="lien-texte">
-        <a href="?action=dialogue/show&type=audio">MAISON Audio</a>
-        <div class="contenu-dialogue" data-type="audio">
-            <h4 class="role">Audio'péra</h4>
-            <p>[Accorde sa lyre] "Ecoutez moi ça ! Pour enregistrer mes ballades, j'utilisais Adobe Audition, mais je fus dégouté lorsque je vis que les Romains l'avais truffé d'abonnements indésirables! Maintenant, avec Audacity, j'ai toute la liberté de créer, c'est un logiciel léger et 100% gratuit ! Je peux y transmettre tous les plus beau accord de ma lyre grâce à NIRD !"</p>
-        </div>
-    </div>
-
-    <div class="lien-texte">
-        <a href="?action=dialogue/show&type=video">MAISON Video</a>
-        <div class="contenu-dialogue" data-type="video">
-            <h4 class="role">La Vate Vidéaste</h4>
-            <p>Quelle horreur ces abonnements indispensables et ces écosystèmes fermés ! L'Empire des GAFAM de César veut contrôler notre créativité ! Pour un Numérique Durable, je passe à KdenLive ! C'est la solution pour mutualiser les ressources et co-construire des outils ouverts avec les autres memebres de la communauté, je ne cautionne pas l'obsolescence programmée ! Surtout quand c'est pour justifier l'achat de la nouvelle version encore plus cher...</p>
-        </div>
-    </div>
-
-    <div class="lien-texte">
-        <a href="?action=dialogue/show&type=rgpd">MAISON RGPD</a>
-        <div class="contenu-dialogue" data-type="rgpd">
-            <h4 class="role">Le Poissonnier de la donnée</h4>
-            <p>Mais ils sont fous ses Romains ! Ils stockent nos données n'importe où, même hors de l'UE? C'est comme le poisson, il faut qu'il soit frais et qu'on sache d'où il vient ! Avec la démarche NIRD, on favorise les solutions locales, ouvertes et autonomes pour un numérique plus éthique! Pas de poisson pas frais ni de données qui se baladent !</p>
-        </div>
-    </div>
-    
-    <div class="lien-texte">
-        <a href="?action=dialogue/show&type=bureautique">HUB</a>
-        <div class="contenu-dialogue" data-type="hub">
-             <h4 class="role">L'HDV NIRD (Explication de NIRD)</h4>
-             <p>Par Bélénos, Bonnemine ! Face à toutes ses licences écrasante de leur prix et de leur poids dans leur marché, ces Romains nous on mis en difficulté, Mais avec le NIRD, le Numérique Inclusif, Responsable et Durable, ces écoles sont à l'image de notre beau village : Irréductible !</p>
-        </div>
-    </div>
-
-    <div class="lien-action">
-        <a href="?action=snake/show">Arbre</a>
-    </div>
-
-    <div class="lien-action">
-        <a href="?action=lasergame/show">Cible</a>
-    </div>
-
+    <a href="?action=dialogue/show&type=bureautique">MAISON bureautique</a>
+    <a href="?action=dialogue/show&type=os">MAISON OS</a>
+    <a href="?action=dialogue/show&type=dessin">MAISON Dessin</a>
+    <a href="?action=dialogue/show&type=ide">MAISON IDE</a>
+    <a href="?action=dialogue/show&type=audio">MAISON Audio</a>
+    <a href="?action=dialogue/show&type=video">MAISON Video</a>
+    <a href="?action=snake/show">Arbre</a>
+    <a href="?action=lasergame/show">Cible</a>
 </div>
 
 
