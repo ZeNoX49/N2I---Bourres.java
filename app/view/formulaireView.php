bb4c672e24a2a6cf0b7374b791cbcaa61bc32584
Nafy <brewen.chausse.fr@gmail.com>
2025-12-05 05:58:06 +0100

    Kdo t'au ma

--- app/view/formulaireView.php
+++ app/view/formulaireView.php
@@ -2,22 +2,28 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Formulaire</title>
-    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/formlaire.css">
+    <title>Formulaire Modale</title>
 </head>
 <body>
 
-<div class="container">
-        <button class="btn-dialog" data-perso="Panoramix">
-            coucou panoramix
-        </button>
+<div id="modal-overlay">
+    <div id="modal-box">
+        <div class="ligne">
+            <input type="text" placeholder="nom">
+        </div>
+        <div class="ligne">
+            <input type="email" placeholder="mail">
+        </div>
+        <div class="ligne">
+            <input type="text" placeholder="sujet">
+        </div>
+        <div class="ligne">
+            <textarea rows="3" placeholder="message"></textarea>
+        </div>
+        <button id="envoyer-btn">envoyer</button>
+    </div>
 </div>
-
-<dialog id="monPopup">
-    <h2 id="popupTitre"></h2>
-    <div id="popupTexte"></div>
-    <form method="dialog"><button>Fermer</button></form>
-</dialog>
 
 </body>
 </html>
