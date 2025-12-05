34ccdc11169a858ecb162a49cd23f3cfac4ffd98
Yfufri <theolannier50@gmail.com>
2025-12-05 03:47:22 +0100

    2eme maison

--- app/view/home.php
+++ app/view/home.php
@@ -12,12 +12,12 @@
 
 <div class="container">
     <button class="btn-dialog mide" data-perso="mide">
-        <img src="assets/img/monTheoAMOA.jpg"
-             alt="Ouvrir le dialogue de Panoramix"
-             width="100" height="100">
+        <img src="assets/image/maison-violette.png"
+             alt="Ouvrir le dialogue sur les logiciels de Bureautique"
+             width="88" height="136">
     </button>
     <button class="btn-dialog mos" data-perso="mos">
-        <img src="assets/img/monTheoAMOA.jpg"
+        <img src="assets/image/maison-bleue.png"
              alt="Ouvrir le dialogue de Assurancetourix"
              width="100" height="100">
     </button>
@@ -39,7 +39,7 @@
     <button class="btn-dialog mnird" data-perso="mnird">
         <img src="assets/img/monTheoAMOA.jpg"
              alt="Ouvrir le dialogue de Assurancetourix"
-             width="100" height="100">
+             width="92" height="130">
     </button>
 </div>
 
